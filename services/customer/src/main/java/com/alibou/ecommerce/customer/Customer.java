d7d1cfd4e3ec924c7e2bac2820747a397e88e954
Harsha6142611 <pasupulaharsha7006@gmail.com>
2025-02-15 18:31:20 +0530

    customer microservice is added

---
+++ services/customer/src/main/java/com/alibou/ecommerce/customer/Customer.java
@@ -0,0 +1,68 @@
+package com.alibou.ecommerce.customer;
+
+import jdk.jshell.Snippet;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Customer {
+
+  @Id
+  private String id;
+
+//  public String getId() {
+//    return id;
+//  }
+//
+//  public void setId(String id) {
+//    this.id = id;
+//  }
+//
+//  public String getFirstname() {
+//    return firstname;
+//  }
+//
+//  public void setFirstname(String firstname) {
+//    this.firstname = firstname;
+//  }
+//
+//  public String getLastname() {
+//    return lastname;
+//  }
+//
+//  public void setLastname(String lastname) {
+//    this.lastname = lastname;
+//  }
+//
+//  public String getEmail() {
+//    return email;
+//  }
+//
+//  public void setEmail(String email) {
+//    this.email = email;
+//  }
+//
+//  public Address getAddress() {
+//    return address;
+//  }
+//
+//  public void setAddress(Address address) {
+//    this.address = address;
+//  }
+
+  private String firstname;
+  private String lastname;
+  private String email;
+  private Address address;
+}
